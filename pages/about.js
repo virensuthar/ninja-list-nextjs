def3d277d7204a99e499c86e4eaebd3719454390
virensuthar <virensuthar5879@gmail.com>
2021-02-21 14:16:14 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | Home</title>
+				<meta name='keywords' content='ninjas' />
+			</Head>
+
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Quisquam quae error excepturi facilis dolore voluptas blanditiis
+					recusandae ipsam obcaecati facere.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Quisquam quae error excepturi facilis dolore voluptas blanditiis
+					recusandae ipsam obcaecati facere.
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default About;
